fa1cd91aedd9ebd71d6a4875bbe34bbfd57f05e5
kvss369 <karnvssandhu@gmail.com>
2026-05-03 01:07:30 -0400

    Create data_fetcher.py for fetching candlestick data from broker APIs

---
+++ data/data_fetcher.py
@@ -0,0 +1,9 @@
+# data_fetcher.py
+
+class DataFetcher:
+    def __init__(self, api_client):
+        self.api_client = api_client
+
+    def fetch_candlestick_data(self, symbol, interval, start_time, end_time):
+        # Implementation for fetching candlestick data from broker APIs
+        pass
